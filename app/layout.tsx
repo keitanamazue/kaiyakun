a5b43cc9912d7b925085af13050d88203c0c1ac8
keitanamazue <keitanamazue@gmail.com>
2025-02-20 00:38:44 +0900

    meta tagの修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
-import type React from "react"
-import "./globals.css"
-import type { Metadata } from "next"
-import { Noto_Sans_JP } from "next/font/google"
+import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
+import type React from "react";
+import "./globals.css";
 
-const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] })
+const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "サブスクリプション解約ハブ",
-  description: "各種サブスクリプションサービスの解約ページへのクイックリンク",
-}
+	title: "サブスク解約",
+	description: "面倒なサブスクの解約ページに一瞬で。",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="ja">
-      <body className={`${notoSansJP.className} bg-orange-50`}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="ja">
+			<body className={`${notoSansJP.className} bg-orange-50`}>{children}</body>
+		</html>
+	);
 }
-
